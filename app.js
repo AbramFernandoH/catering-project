fb489c9b9a2c246ce40c9fc7425f1cac19a01262
AbramFernandoH <abramfernando18@gmail.com>
2021-04-23 17:36:23 +0700

    Registering a user with passport js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const moment = require('moment');
 const engine = require('ejs-mate');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const flash = require('connect-flash');
 
 // native modules
 const path = require('path');
@@ -13,7 +18,11 @@ const User = require('./model/user');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/catering-project', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/catering-project', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.log.bind(console, "Connection error!"));
+db.once('open', () => console.log('Database connected'));
 
 app.engine('ejs', engine);
 
@@ -22,6 +31,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(session({ secret: 'RahasiaaaSekali', resave: false, saveUninitialized: true, cookie: {
+  expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+  maxAge: 1000 * 60 * 60 * 24 * 7
+}
+}));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
   res.render('home', { headTitle: 'Home', navLinks: [ 'About Us', 'Menu This Week', 'Testimoni', 'Order', 'Contact Us', 'Log In' ] });
@@ -33,6 +56,15 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('section/register', { headTitle: 'Register', navLinks: [ 'Home', 'Login' ] });
+});
+
+app.post('/register', async (req, res) => {
+  const { username, email, password, address } = req.body;
+  const user = new User({ username, email, address });
+  const newUser = await User.register(user, password);
+  req.login(newUser, () => {
+    res.redirect('/');
+  });
 });
 
 app.get('/order', (req, res) => {
--- model/user.js
+++ model/user.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true
   },
-  password: String,
   address: {
     type: String,
     required: true
   }
 });
+
+// adding username, hash and salt from original password
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
